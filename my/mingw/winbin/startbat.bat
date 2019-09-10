938a8ab52999d191005fd7703b53562cc667832a
ambiesoft <ambiesoft.trueff@gmail.com>
2019-09-10 12:24:07 +0900

    fixsome

--- my/mingw/winbin/startbat.bat
+++ my/mingw/winbin/startbat.bat
@@ -1,9 +1,29 @@
 
+@echo off
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+) else if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+  call "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+) else (
+  echo "Visual Studio not found"
+  goto :end
+)
 
+setlocal
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+  set MSYSROOT=C:\local\msys2
+  set MINGWROOT=mingw64
+) else if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+  set MSYSROOT=C:\local\msys2
+  set MINGWROOT=mingw32
+) else (
+  echo "Visual Studio not found"
+  goto :end
+)
+endlocal & set PATH=%MSYSROOT%\%MINGWROOT%\bin;%MSYSROOT%\usr\bin;%PATH%
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-
-set MSYSROOT=C:\local\msys2
-set PATH=%MSYSROOT%\mingw64\bin;%MSYSROOT%\usr\bin;%PATH%
 cd ..\..\..
+
+
+:end
 cmd /k
