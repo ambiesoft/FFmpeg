0c60f3dae2cb55fb1d9657466a32e35e3db107ca
ambiesoft <ambiesoft.trueff@gmail.com>
2020-12-30 14:34:01 +0900

    nazo

--- my/mingw/winbin/startbat.bat
+++ my/mingw/winbin/startbat.bat
@@ -10,11 +10,10 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\
 )
 
 setlocal
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
   set MSYSROOT=C:\local\msys2
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
   set MINGWROOT=mingw64
 ) else if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
-  set MSYSROOT=C:\local\msys2
   set MINGWROOT=mingw32
 ) else (
   echo "Visual Studio not found"
@@ -22,8 +21,14 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\
 )
 endlocal & set PATH=%MSYSROOT%\%MINGWROOT%\bin;%MSYSROOT%\usr\bin;%PATH%
 
+:: Include ffmpeg header directory
+set INCLUDE=%INCLUDE%;C:\local\msys2\opt\ffmpeg-i686-w64-mingw32\include
+
+:: cd to dev src root
 cd ..\..\..
 
 
-:end
+devenv C:\local\msys2\opt\ffmpeg-x86_64-w64-mingw32\bin\ffplay.sln
 cmd /k
+
+:end
